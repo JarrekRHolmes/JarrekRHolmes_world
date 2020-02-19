d2580d27913d2c8b8c7595bba4d469f4bc69206d
Vivian Huang <vhuang13@gmail.com>
2020-02-18 22:46:52 -0800

    Revert "done"
    This reverts commit c7ea1775642ffc6f2f71fede772c6ca997538615.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   resources :photos
   resources :proverbs
   resources :categories
-  resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
